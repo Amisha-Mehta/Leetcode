809bdc5e31443c62a8504d5778b6289d09470a06
Amisha Mehta <145385847+Amisha-Mehta@users.noreply.github.com>
2026-04-23 00:53:00 +0530

    added solution for best-time-to-buy-and-sell-stock

---
+++ best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int buy=prices[0];
+        int sell=0;
+        
+        for(int i=1;i<n;i++){
+            buy=min(buy,prices[i]);
+            sell=max(sell,prices[i]-buy);
+        }
+        return sell;
+    }
+};
