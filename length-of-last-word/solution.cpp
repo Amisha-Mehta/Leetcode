3fd00832b8c4325ba339048839fffa2d59439a37
Amisha Mehta <145385847+Amisha-Mehta@users.noreply.github.com>
2026-06-23 18:04:31 +0530

    added solution for length-of-last-word

--- length-of-last-word/solution.cpp
+++ length-of-last-word/solution.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int end=s.length()-1;
+        int n=s.length();
+        string str="";
+        for(int i=n-1;i>=0;i--){
+            if(str.empty() && s[i]==' '){
+                continue;
+            }
+            if(s[i]==' ')
+                break;
+            str+=s[i];
+        }
+        return str.length();
 
-        while(end>=0 && s[end]==' '){
-            end--;
-        }
-        int start=end;
-        while(start>=0 && s[start]!=' '){
-            start--;
-        }
-        return end-start;
+    // string str="";
+    // vector<string> ans;
+    // for(int i=0;i<n;i++){
+    //     if(s[i]!=' '){
+    //         str+=s[i];
+    //     }
+    //     else if(!str.empty()){
+    //         ans.push_back(str);
+    //         str="";
+    //     }
+    // }
+    // if(!str.empty()){
+    //     ans.push_back(str);
+    // }
+    // return ans[ans.size()-1].size();
     }
 };
