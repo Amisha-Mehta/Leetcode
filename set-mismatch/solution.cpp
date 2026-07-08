29439db72542a9f8bc25489962226d26d46c1ff5
Amisha Mehta <145385847+Amisha-Mehta@users.noreply.github.com>
2026-07-08 16:30:49 +0530

    added solution for set-mismatch

---
+++ set-mismatch/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> freq(n+1,0);
+        int dup=-1,miss=-1;
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(freq[i]==0) miss=i;
+            if(freq[i]==2) dup=i;
+        }
+        return {dup,miss};
+    }
+};
